b99ea54ac307d44c0d0b8f6a9923e81845aa1823
David Oberlander <david.oberlander@student.cvtc.edu>
2022-04-01 20:20:02 -0500

    Test response for / route.

--- index.js
+++ index.js
@@ -14,13 +14,14 @@ express()
   .use(express.static(path.join(__dirname, 'public')))
   .use(express.json())
   .use(express.urlencoded({ extended: true }))
+  .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', async(req, res) => {
     try{
     const client = await pool.connect();
 
     client.release();
-    res.send("Works");
+      res.send("Works");
     } catch (err) {
       console.error(err);
       res.send("Error " + err);
